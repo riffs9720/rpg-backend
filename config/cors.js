6376fa369bba226ef56edbf2e495849ff469b49c
riffs9720 <ricardo.silva@saojudas.br>
2021-09-20 14:57:37 -0300

    Alteraçao de cors

--- config/cors.js
+++ config/cors.js
@@ -21,7 +21,7 @@ module.exports = {
 //   return requestOrigin === 'https://rpg-frontend.herokuapp.com'
 // },
 
-origin: false,
+origin: true,
 
   /*
   |--------------------------------------------------------------------------
