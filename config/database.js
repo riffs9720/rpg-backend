b0e919053b2e4d9f3b3c9a39f3d158ee75851753
riffs9720 <ricardo.silva@saojudas.br>
2021-09-20 12:43:45 -0300

    configurando ssl

--- config/database.js
+++ config/database.js
@@ -81,12 +81,12 @@ pg: {
       port: Env.get('DB_PORT', DATABASE_URL.port),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
+      extra: {
+        ssl: true
+   }
     },
     debug: Env.get('DB_DEBUG', false),
-    ssl: {
-      rejectUnauthorized: false
-    }
   }
 } 
 // pg: {
