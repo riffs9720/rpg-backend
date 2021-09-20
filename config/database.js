5be4e28fd351d55d985809da934a4b6b0571d90f
riffs9720 <ricardo.silva@saojudas.br>
2021-09-20 12:46:24 -0300

    configurando ssl

--- config/database.js
+++ config/database.js
@@ -82,9 +82,7 @@ pg: {
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
-      extra: {
-        ssl: true
-   }
+      ssl: true,
     },
     debug: Env.get('DB_DEBUG', false),
   }
